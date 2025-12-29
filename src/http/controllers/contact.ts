9faa3f4154095e5f11c80dd29cf8d20f95d83782
Pranjal <pranjalchabra0@gmail.com>
2025-12-29 22:03:54 +0530

    http controller completed

---
+++ src/http/controllers/contact.ts
@@ -0,0 +1,39 @@
+// return a array of all the registered contact of a user
+import { object, z } from "zod";
+import { Prisma } from "../utility/prismaClient.js";
+const listUserContactReqSchema = z.object(
+  {
+    user: z.object({ userId: z.number("id is not present") }),
+  },
+  "not a error"
+);
+export async function listUserContact(req, res) {
+  try {
+    const {
+      user: { userId: id },
+    } = listUserContactReqSchema.parse(req);
+    // validation using zod schema
+    // find the user in user table the user relation to get all the contact of the user to then user those contacts to find all the user name of those contacts
+    const findUser = await Prisma.user.findUnique({
+      where: { id },
+      include: {
+        contactsOfUser: {
+          select: {
+            contact: { select: { username: true, id: true } },
+          },
+        },
+      },
+    });
+    const contacts = findUser?.contactsOfUser?.map((x) => x.contact);
+    if (typeof contacts != "undefined") {
+      return res.json({ contacts });
+    }
+    return res.status(404).json({ error: "no contact found" });
+  } catch (e) {
+    if (e instanceof Error) {
+      return res.status(500).json({ error: e.message });
+    } else {
+      return res.status(500).json({ error: "internal server error " });
+    }
+  }
+}
---
+++ src/http/controllers/user.ts
@@ -0,0 +1,15 @@
+import { Prisma } from "../utility/prismaClient.js";
+import { z } from "zod";
+async function userInfo(req, res) {
+  try {
+    const userId = z.number().parse(req.user);
+    const foundUser = await Prisma.user.findUnique({
+      where: { id: userId },
+      omit: { password: true },
+    });
+    return res.status(200).json({ message: "user found", data: { foundUser } });
+  } catch (e) {
+    return res.status(400).json({ message: "cant find the user" });
+  }
+}
+//# sourceMappingURL=user.controller.js.map
