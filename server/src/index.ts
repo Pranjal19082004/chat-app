97c6fd802ac4cc736d8124a8828e44025176c464
Pranjal <pranjalchabra0@gmail.com>
2025-12-30 23:00:21 +0530

    feat : wrote a proper index file with configured routes and server listening

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,32 @@
-import { WebSocketServer, type RawData } from "ws";
-import { onConnect } from "./wss/handlers/onConnect.js";
-import router from "./wss/routes/index.js";
-import { z } from "zod";
-import { wsMessageSchema, WsMssgMethodsArray } from "./types/wsTypes.js";
+import express from "express";
+import { createServer } from "http";
+import { config } from "dotenv";
+import cors from "cors";
+config();
+import "./utility/prismaClient.js";
+import initWebSocketServer from "./wss/index.js";
+const app = express();
+//parsing middlewares and cors
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+//routes
+import authRouter from "./http/routes/auth.routes.js";
+app.use("/api/auth", authRouter);
 
-function initWebSocketServer(server: any) {
-  const wss = new WebSocketServer({ server });
-  wss.on("connection", async (ws, req) => {
-    try {
-      await onConnect(ws, req);
-      ws.on("message", (data: RawData) => {
-        const { method, payload } = wsMessageSchema.parse(
-          JSON.parse(data.toString())
-        );
-        router[method]?.(payload, ws);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  });
-  return wss;
-}
-export default initWebSocketServer;
+import { authMiddleware } from "./http/middlewares/authMiddleware.js";
+app.use(authMiddleware);
+import groupRouter from "./http/routes/group.routes.js";
+app.use("/api/group", groupRouter);
+import contactRouter from "./http/routes/contact.routes.js";
+app.use("/api/contact", contactRouter);
+import messageRouter from "./http/routes/message.route.js";
+app.use("/api/message", messageRouter);
+import userRouter from "./http/routes/user.routes.js";
+app.use("/api/user", userRouter);
+const server = createServer(app);
+const wss = initWebSocketServer(server);
+
+server.listen(3000, () => {
+  console.log("server is now listening on PORT: 3000\n");
+});
