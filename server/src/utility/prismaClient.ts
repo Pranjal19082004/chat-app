c3c74c7006bc3091cb21047220710a0f79e19ec8
Pranjal <pranjalchabra0@gmail.com>
2025-12-30 22:48:34 +0530

    configured prisma client with neon adaptor

--- server/src/utility/prismaClient.ts
+++ server/src/utility/prismaClient.ts
@@ -1,2 +1,9 @@
-import { PrismaClient } from "../generated/prisma/index.js";
-export const Prisma = new PrismaClient();
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaClient } from "../../src/generated/prisma/index.js";
+import ws from "ws";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import dotenv from "dotenv";
+dotenv.config(); // Load environment variables
+neonConfig.webSocketConstructor = ws;
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+export const Prisma = new PrismaClient({ adapter });
